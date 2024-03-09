460e286452b1f5affee327553605759dbc433d3b
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-09 09:58:48 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+//project=10
+//Adding Comments: Choose two of the programs you’ve written, and 
+//add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//addition
+console.log(5 + 3);
+//subtraction
+console.log(12 - 4);
+//multipication
+console.log(2 * 4);
+//division
+console.log(16 / 2);
